bd9a649a1960fa7a342a4ecd1ac79e92c21844e4
alu0100886870 <alu0100886870@ull.edu.es>
2019-11-20 18:56:27 +0000

    Subiendo practica de tilemaps

---
+++ Assets/Characters/PlayerControllerIsom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerIsom : MonoBehaviour
+{
+
+    private Animator animator;
+    private SpriteRenderer sp;
+    private Rigidbody2D rb;
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        rb.AddForce(move * speed);
+       // transform.position += move * speed * Time.deltaTime;
+        
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Generación de Tile maps
+---
+**Demostración:**
+1. Generación de tilemaps 2D, con colisiones y personaje movible.
+![](tilemaps1.gif)
+2. Generación de un mapa isométrico, con alturas, colisiones y personaje (mesa).
+![](tilemaps2.gif)
+3. Generación de un mapa hexagonal, con colisiones y personaje.
+![](tilemaps3.gif)
+4. Generación de ruletiles. Es una prueba donde se ha generado varios.
+![](ruletiles.PNG)
